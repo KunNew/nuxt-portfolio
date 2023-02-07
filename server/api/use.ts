7b8106997d936dd037c6a6031d8899e7b08c3784
pheak <kunnew20@gmail.com>
2023-02-07 11:02:24 +0700

    improve

--- server/api/use.ts
+++ server/api/use.ts
@@ -1,18 +1,14 @@
 export default defineEventHandler((event) => {
-    return {
-        uses: [
-            {
-                name: 'Editor',
-                tools: ['VS Code',
-                'Bearded Theme',
-
-                ],
-            },
-            {
-                name: 'Setup',
-                tools: ['huawei D14'],
-            } 
-        ]
-    }
-  });
-  
+  return {
+    uses: [
+      {
+        name: "Editor",
+        tools: ["VS Code", "Bearded Theme"],
+      },
+      {
+        name: "Setup",
+        tools: ["huawei D14"],
+      },
+    ],
+  };
+});
